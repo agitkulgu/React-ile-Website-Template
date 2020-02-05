ed8dc1bf039ae915a778d75ee5bf9499d2edcc5b
Agit Kulgu <agit.kulgu@hogarthww.com>
2020-02-05 11:25:55 +0300

    react kuurulumu ve componentler oluşturuldu

---
+++ website-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Header from "./Components/Header"
+import Content from "./Components/Content"
+import Footer from "./Components/Footer"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ website-app/src/Components/Content.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Content(){
+    return (
+        <content>
+            Content is cached for offline use.
+        </content>
+    )
+}
+export default Content
